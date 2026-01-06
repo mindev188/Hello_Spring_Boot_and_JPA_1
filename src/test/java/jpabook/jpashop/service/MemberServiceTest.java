2425dfcb8b03fe27fcf51c4165a5157ce7620c24
mindev188 <mindev188@gmail.com>
2026-01-06 21:06:02 +0900

    feat: 회원기능 테스트 (jUnit5, h2 mem)

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -7,6 +7,7 @@ import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +20,7 @@ public class MemberServiceTest {
     @Autowired EntityManager em;
 
     @Test
+    @Rollback(false)
     public void 회원가입() throws Exception {
         // GIVEN
         Member member = new Member();
@@ -26,7 +28,7 @@ public class MemberServiceTest {
 
         // WHEN
         Long saveId = memberService.join(member);
-        
+
         // THEN
         em.flush();
         assertEquals(member, memberRepository.findOne(saveId));
@@ -36,9 +38,16 @@ public class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception {
         // GIVEN
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
 
         // WHEN
+        memberService.join(member1);
 
         // THEN
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
     }
 }
