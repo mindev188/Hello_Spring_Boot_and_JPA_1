2d042ca3467160d67151f41dfa5eebc163b477d8
mindev188 <mindev188@gmail.com>
2026-01-06 12:56:41 +0900

    Revert "feat: 회원 서비스 개발 (MemberService)"
    This reverts commit 84139a2483c73c01593d4201aa359c4fa1aebe0b.

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,17 +3,15 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class MemberRepository {
 
-//    @PersistenceContext
-    private final EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
